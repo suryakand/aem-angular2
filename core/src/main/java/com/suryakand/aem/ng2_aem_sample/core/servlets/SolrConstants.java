fe8a1e1964dbf68a9daf822d6bb61b204dcb1fa3
suryakand <suryakand.shinde@.kp.org>
2017-06-25 15:33:39 -0700

    Added basic SolrSearchServlet to fetch search result and format it using wmc.io libraries that will be displyed on UI

---
+++ core/src/main/java/com/suryakand/aem/ng2_aem_sample/core/servlets/SolrConstants.java
@@ -0,0 +1,7 @@
+package com.suryakand.aem.ng2_aem_sample.core.servlets;
+public class SolrConstants {
+  
+  public static final String PATH_DES = "path_des";
+  public static final String PATH_EXACT = "path_exact";
+  public static final String PATH_COLLAPSED = "path_collapsed";
+}
