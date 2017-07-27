14c6d64b74816c288f1ca49168f2ce64b4209d8c
Ian Reasor <ian.reasor@gmail.com>
2017-07-27 11:56:47 -0700

    Code building and deploying, test content page rendering. Updated dependencies to work on AEM 6.3. Removed unnecessary dependencies on Neba. Removed unused it.tests and it.launcher projects. Removed sample code from the core module.

--- core/src/main/java/com/suryakand/aem/ng2_aem_sample/core/servlets/SolrConstants.java
+++
@@ -1,7 +0,0 @@
-package com.suryakand.aem.ng2_aem_sample.core.servlets;
-public class SolrConstants {
-  
-  public static final String PATH_DES = "path_des";
-  public static final String PATH_EXACT = "path_exact";
-  public static final String PATH_COLLAPSED = "path_collapsed";
-}
