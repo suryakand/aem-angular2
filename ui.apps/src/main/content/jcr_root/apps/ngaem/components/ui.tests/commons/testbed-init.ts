af79f22ba1b03fce1bb448866a17880984b812c7
suryakand <suryakand.shinde@.kp.org>
2018-01-26 14:26:59 -0800

    Added AboutComponent unit testing example

---
+++ ui.apps/src/main/content/jcr_root/apps/ngaem/components/ui.tests/commons/testbed-init.ts
@@ -0,0 +1,38 @@
+import { FormsModule } from '@angular/forms';
+import { MockBackend } from "@angular/http/testing";
+import { Http, ConnectionBackend, BaseRequestOptions, XHRBackend, ResponseOptions, Response } from "@angular/http";
+
+export class CLTestBed {
+    private climports:any = [];
+    private cldeclarations:any = [];
+    private clproviders:any = [];
+
+    constructor() {
+        this.climports = [FormsModule];
+        this.clproviders = [
+            MockBackend,
+            BaseRequestOptions,
+            { provide: Http, useFactory: (backend: MockBackend, defaultOptions: BaseRequestOptions) => {
+                    return new Http(backend, defaultOptions);
+                }, deps: [MockBackend, BaseRequestOptions]
+            }
+        ];
+    }
+
+    public getTestBed(declarations?:any, providers?:any) {
+        let ci = this;
+        if (providers) {
+            ci.clproviders.push(providers);
+        }
+
+        if (declarations) {
+            ci.cldeclarations.push(declarations);
+        }
+
+        return {
+            imports: ci.climports,
+            declarations: ci.cldeclarations,
+            providers: ci.clproviders
+        };
+    }
+}
---
+++ ui.apps/src/main/content/jcr_root/apps/ngaem/components/ui.tests/components/about.component.spec.ts
@@ -0,0 +1,41 @@
+import { MockBackend } from "@angular/http/testing";
+import { TestBed, tick, async, fakeAsync, inject } from "@angular/core/testing";
+import { Observable } from 'rxjs/Observable';
+import { ElementRef} from "@angular/core";
+import 'rxjs/add/operator/map';
+import 'rxjs/add/observable/of';
+import {AboutComponent} from "../../content/about/about.component";
+import {TextAreaComponent} from "../../content/text-area/text-area.component";
+
+import { CLTestBed } from '../commons/testbed-init';
+
+export class MockElementRef extends ElementRef {}
+
+describe("Validate AboutComponent", () => {
+
+    describe("Is Redered with H1 title as About", () => {
+
+        beforeEach(async(() => {
+            TestBed.configureTestingModule(new CLTestBed().getTestBed([AboutComponent, TextAreaComponent], { provide: ElementRef, useClass: MockElementRef })).compileComponents();
+        }));
+
+        it('Render Component', inject([], fakeAsync(() => {
+            const fixture = TestBed.createComponent(AboutComponent);
+            let element = fixture.nativeElement;
+            let component = fixture.componentInstance;
+            fixture.detectChanges();
+            tick();
+
+            // Check that AboutComponent is rendered
+            expect(element).not.toBeNull();
+            expect(component).not.toBeNull();
+
+            // Grab referece of H1 element of AboutComponent
+            let titleElement = element.querySelector("h1");
+            expect(titleElement).not.toBeNull();
+            // Check that H1 text is "About"
+            expect(titleElement.textContent).toEqual("About");
+        })));
+    });
+
+});
